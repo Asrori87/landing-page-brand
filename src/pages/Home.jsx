6bbb805b29b13f091f7bef5f5da2b6665ef3bf0a
Asrori <fauzanasrori87@gmail.com>
2024-11-03 19:28:21 +0000

    landing page fiktif brand

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Category from "../components/Category";
+import Header from "../components/Header";
+import ProductLists from "../components/ProductLists";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <ProductLists />
+      <Category />
+    </>
+  );
+}
